b79fa32e58d91075be8253e7d593251426bfc152
Confetti7 <243691485@qq.com>
2019-04-09 15:19:55 +0800

    css map

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -63,7 +63,7 @@ module.exports = merge(base, {
                 exclude: /\.min\.js$/, // 过滤掉以".min.js"结尾的文件，默认这个后缀本身就是已经压缩好的代码，没必要进行二次压缩
                 cache: true,
                 parallel: true, // 开启并行压缩，充分利用cpu
-                sourceMap: false,
+                sourceMap: true,
                 extractComments: false,
                 uglifyOptions: {
                     compress: {
@@ -87,6 +87,10 @@ module.exports = merge(base, {
             new OptimizeCssAssetsPlugin({
                 assetNameRegExp: /\.css$/g,
                 cssProcessorOptions: {
+                    map: {
+                        inline: false,
+                        annotation: true,
+                    },
                     safe: true,
                     autoprefixer: { disable: true }, // 禁用掉cssnano对于浏览器前缀的处理 否则会把autoprefixer加好的前缀去掉
                     mergeLonghand: false,
