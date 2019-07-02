a1aa29f3e7e043af668e60dd18995606b821267f
Confetti7 <243691485@qq.com>
2019-07-02 20:18:17 +0800

    升级react；lazy替换react-loadable

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
 import { AppContainer } from 'react-hot-loader';
 
 import { Router, Switch } from 'dva/router';
 import { Route } from '../components';
+import Loading from '../components/Loading';
 import routes from './routes';
 
 import BasicLayout from '../containers/BasicLayout';
@@ -13,44 +14,40 @@ import NoMatch from '../pages/404';
 export default function router({ history }) {
     return (
         <AppContainer>
-            <Router
-                history={history}
-                getUserConfirmation={(message, callback) => {
-                    const allowTransition = window.confirm(message);
-                    callback(allowTransition);
-                }}
-            >
-                <Switch>
-                    {routes.map(item => (
-                        <Route
-                            key={item.key}
-                            path={item.path}
-                            exact={item.exact}
-                            render={(matchProps) => {
-                                const matchRoute = routes.filter(route => route.path === matchProps.match.path)[0];
-                                document.title = matchRoute.title;
-                                // 登录重定向逻辑
+            <Router history={history}>
+                <Suspense fallback={<Loading />}>
+                    <Switch>
+                        {routes.map(item => (
+                            <Route
+                                key={item.key}
+                                path={item.path}
+                                exact={item.exact}
+                                render={(matchProps) => {
+                                    const matchRoute = routes.filter(route => route.path === matchProps.match.path)[0];
+                                    document.title = matchRoute.title;
+                                    // 登录重定向逻辑
 
-                                return (
-                                    <BasicLayout {...matchProps}>
-                                        <item.component {...matchProps} />
-                                    </BasicLayout>
-                                );
-                            }}
-                            onEnter={(props) => {
-                                console.log('onEnter', props);
-                            }}
-                            onUpdate={(props, nextProps) => {
-                                console.log('onUpdate: props', props);
-                                console.log('onUpdate: nextProps', nextProps);
-                            }}
-                            onLeave={(props) => {
-                                console.log('onLeave', props);
-                            }}
-                        />
-                    ))}
-                    <Route component={NoMatch} />
-                </Switch>
+                                    return (
+                                        <BasicLayout {...matchProps}>
+                                            <item.component {...matchProps} />
+                                        </BasicLayout>
+                                    );
+                                }}
+                                onEnter={(props) => {
+                                    console.log('onEnter', props);
+                                }}
+                                onUpdate={(props, nextProps) => {
+                                    console.log('onUpdate: props', props);
+                                    console.log('onUpdate: nextProps', nextProps);
+                                }}
+                                onLeave={(props) => {
+                                    console.log('onLeave', props);
+                                }}
+                            />
+                        ))}
+                        <Route component={NoMatch} />
+                    </Switch>
+                </Suspense>
             </Router>
         </AppContainer>
     );
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,4 +1,4 @@
-import loadable from './loadable';
+import { lazy } from 'react';
 
 // 路由权限过滤
 export default [
@@ -6,14 +6,14 @@ export default [
         title: '首页',
         key: '/',
         path: '/',
-        component: loadable('Home'),
+        component: lazy(() => import(/* webpackChunkName: "Home" */ '../pages/Home')),
         exact: true,
     },
     {
         title: 'cNode列表',
         key: '/look',
         path: '/look/:id',
-        component: loadable('Look'),
+        component: lazy(() => import(/* webpackChunkName: "Look" */ '../pages/Look')),
         needLogin: true,
     },
 ];
